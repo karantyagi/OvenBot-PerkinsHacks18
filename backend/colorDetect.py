fe54d10a4b8f1c78189fe4e736e623f591c8adb1
karantyagi <tyagi.k@husky.neu.edu>
2018-04-14 11:37:10 -0400

    Updating colorDetect

--- backend/colorDetect.py
+++ backend/colorDetect.py
@@ -1,18 +1,197 @@
 
 ## Real time video from webcamera
 
-
+from __future__ import division
+from google.cloud import vision
 import numpy as np
 import cv2
 import win32com.client as wincl
+import time
+import base64
+import requests
+import json
+import cv2
+import io
+from google.cloud.vision import types
+import numpy as np
+from sklearn.metrics.pairwise import euclidean_distances as ED
+import math
+import sys
+import os
 
+input = sys.argv[1]
+
+# path to google APi json file
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="googleAPI/perkins-hackathon-OCR-b116d4935f42.json"
+
+
+def detect_text(path):
+    centroids_list = []
+    text_list = []
+    cordinates_list = []
+
+    """Detects text in the file."""
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = types.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    print('Texts:')
+
+    for text in texts:
+        print('\n"{}"'.format(text.description))
+
+        vertices = (['({},{})'.format(vertex.x, vertex.y)
+                    for vertex in text.bounding_poly.vertices])
+        temp_list = []
+        for vertex in text.bounding_poly.vertices:
+            temp_list.append((vertex.x,vertex.y))
+        cordinates_list.append(temp_list)
+        vertices_temp = [(each.x,each.y) for each in text.bounding_poly.vertices]
+        c_x= 0
+        c_y= 0
+        for each in vertices_temp:
+            c_x+= each[0]
+            c_y+= each[1]
+        c_x/=4
+        c_y/=4
+        centroids_list.append((c_x,c_y))
+        text_list.append(text.description)
+
+        print('bounds: {}'.format(','.join(vertices)))
+    return centroids_list,text_list,cordinates_list
+
+
+def get_euclidean_distance(x,y):
+    return(math.pow(x[0]-y[0],2)+math.pow(x[1]-y[1],2))
+
+
+def get_merging_indices(c):
+    l = len(c)
+    min_dist =0
+    if(l>2):
+        min_dist = get_euclidean_distance(c[1],c[2])
+        for i in range(1,l):
+            for j in range(1,l):
+                if(i!=j):
+                    if(get_euclidean_distance(c[i],c[j]) < min_dist):
+                        min_dist = get_euclidean_distance(c[i],c[j])
+    merging_indices = []
+    for i in range(1,l):
+            for j in range(1,l):
+                if(i!=j):
+                    if(get_euclidean_distance(c[i],c[j]) < (1.20)*min_dist):
+                        if((i,j) not in merging_indices and (j,i) not in merging_indices):
+                            merging_indices.append((i,j))
+    return merging_indices
+
+def merge_(indices,coords,t):
+    temp_coords = []
+    temp_t = []
+    for each in indices:
+        x1 = [vertex[0] for
+              vertex in coords[each[0]]]
+#         print(coords[each[1]])
+        x2 = [vertex[0] for vertex in coords[each[1]]]
+        y1 = [vertex[1] for vertex in coords[each[0]]]
+        y2 = [vertex[1] for vertex in coords[each[1]]]
+        x_min =min([min(x1),min(x2)])
+        x_max = max([max(x1),max(x2)])
+        y_min = min([min(y1),min(y2)])
+        y_max = max([max(y1),max(y2)])
+        temp_coords.append([(x_min,y_min),(x_max,y_min),(x_max,y_max),(x_min,y_max)])
+        temp_t.append(t[each[0]]+" "+t[each[1]])
+#         coords.pop(each[0])
+#         coords.pop(each[1])
+    return temp_coords,temp_t
+
+
+def get_merged_lists(merging_indices,coords,t):
+    delete_indices = []
+    for each in merging_indices:
+        delete_indices.append(each[0])
+        delete_indices.append(each[1])
+    new_t = []
+    new_coords = []
+    for i in range(len(coords)):
+        if(i not in delete_indices):
+            new_coords.append(coords[i])
+            new_t.append(t[i])
+    return new_t,new_coords
+
+
+def get_centroid(coords,ind):
+    avg_x=0
+    avg_y=0
+    for each in coords[ind]:
+        avg_x+=each[0]
+        avg_y+=each[1]
+    return((avg_x/4,avg_y/4))
+
+def is_inside(curr,goal_ind,new_coords):
+    x = curr[0]
+    y = curr[1]
+    ax = new_coords[goal_ind][0][0]
+    ay = new_coords[goal_ind][0][1]
+
+    bx = new_coords[goal_ind][1][0]
+    by = new_coords[goal_ind][1][1]
+
+    dx = new_coords[goal_ind][2][0]
+    dy = new_coords[goal_ind][2][1]
+
+    bax = bx - ax
+    bay = by - ay
+    dax = dx - ax
+    day = dy - ay
+
+    if ((x - ax) * bax + (y - ay) * bay < 0.0):
+        return 0
+    if ((x - bx) * bax + (y - by) * bay > 0.0):
+        return 0
+    if ((x - ax) * dax + (y - ay) * day < 0.0):
+        return 0
+    if ((x - dx) * dax + (y - dy) * day > 0.0):
+        return 0
+
+    return 1
+
+def gps(curr,goal_ind,coords):
+    if(is_inside(curr,goal_ind,coords)==1):
+        print('Reached!')
+        return('Reached')
+    else:
+        cent = get_centroid(coords,goal_ind)
+        ans = ""
+        if(curr[1]>=coords[goal_ind][0][1] and curr[1]<=coords[goal_ind][2][1] ):
+            ans+=""
+        elif(cent[1]>curr[1]):
+            ans+="down"
+        elif(cent[1]<curr[1]):
+            ans+="up"
+        if(curr[0]>=coords[goal_ind][0][0] and curr[0]<=coords[goal_ind][1][0] ):
+            ans+=""
+        elif(cent[0]>curr[0]):
+            ans+=" right"
+        elif(cent[0]<curr[0]):
+            ans+=" left"
+        print(ans)
+        return(ans)
+
+#########
+
+## time.sleep(5)   # delays for 5 seconds. You can Also Use Float Value.
 
 # parameters
 # Constants for finding range of skin color in YCrCb
 timer = 0
 fps = 30
 
-def getImage():
+def startLoop():
     #cv2.namedWindow("webcam-feed")
     cam = cv2.VideoCapture(0)
 
@@ -26,6 +205,7 @@ def getImage():
     while ret:
 
         ret, frame = cam.read()
+        frame = cv2.flip(frame,1)
         #color = np.uint8([[[blue, green, red]]])
         #hsv_color = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -98,8 +278,10 @@ def getImage():
 
         frames+=1
         timer = frames/fps
-        if timer % 3 == 0:
-            speak_out = "GO LEFT"
+        if timer % 4 == 0:
+            curr = (x,y)
+
+            speak_out = gps(curr,3,new_coords1)
             speak = wincl.Dispatch("SAPI.SpVoice")
             speak.Speak(speak_out)
             print("Bot spoke! | msg: {} at time : {} sec".format(speak_out,timer))
@@ -122,4 +304,30 @@ def getImage():
     cv2.destroyAllWindows()
     cv2.destroyWindow("yellow")
 
-getImage()
+
+############## START RUN ###########
+
+path1 = 'testImages/opencv_frame_0.png'
+c1,t1,coords1 = detect_text(path1)
+merging_indices1 = get_merging_indices(c1)
+# coords = list(original_coords)
+# t = list(original_t)
+temp_coords,temp_t = merge_(merging_indices1,coords1,t1)
+coords1.extend(temp_coords)
+t1.extend(temp_t)
+
+new_t1 = []
+new_coords1 = []
+new_t1,new_coords1  = get_merged_lists(merging_indices1,coords1,t1)
+
+
+if input.lower() == "start":
+    startLoop() ### start infinite loop
+
+
+'''
+# Standard boilerplate to call the main() function to begin
+# the program.
+if __name__ == '__main__':
+  main()
+  '''
